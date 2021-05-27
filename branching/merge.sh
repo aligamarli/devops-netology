9f4b02ad8872e19e21d38b1668d8699a5ddfaeba
Aligamarli Namazov <aligamarli@gmail.com>
2021-05-27 15:54:14 +0400

    rebase.sh

--- branching/merge.sh
+++ branching/merge.sh
@@ -3,7 +3,8 @@
 
 count=1
 while [[ -n "$1" ]]; do
-    echo "Parameter #$count = $1"
-    count=$(( $count + 1 ))
-    shift
+     echo "\$@ Parameter #$count = $param"
+     count=$(( $count + 1 ))
+     shift
 done
+
